dd413ea67e025034b4eef5b759a534e4f19f4eac
dimyG <dimgeows@gmail.com>
2023-03-06 18:20:58 +0200

    small changes for ecs task creation

--- payments_prj/settings/prod.py
+++ payments_prj/settings/prod.py
@@ -4,5 +4,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
+# todo: remove cors headers wehn you set up a custom domain name for the API Gateway
+INSTALLED_APPS.insert(0, 'corsheaders')
+MIDDLEWARE.insert(0, 'corsheaders.middleware.CorsMiddleware')  # not ASGI compatible
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
